239ff0996d3dba5924dbc55af01604bc327d9b38
Christian Gustavsson <christian.gustavsson@nordicfeel.se>
2024-05-05 17:51:02 +0200

    Added new Test

--- DeveloperTest.Tests/ExtensionMethodTests.cs
+++ DeveloperTest.Tests/ExtensionMethodTests.cs
@@ -3,11 +3,18 @@ namespace DeveloperTest.Tests
 	public class ExtensionMethodTests
 	{
 		[Fact]
-		public void ExtensionMethods_Add_ReturnSum()
+		public void ExtensionMethods_Add_ReturnSum_OK()
 		{
 			int sum = ExtensionMethods.Add(12, 13);
 
 			Assert.Equal(25, sum);
+
+		}
+
+		[Fact]
+		public void ExtensionMethods_Add_ReturnSum_NotOK()
+		{
+			int sum = ExtensionMethods.Add(12, 13);
 
 			Assert.NotEqual(1, sum);
 		}
